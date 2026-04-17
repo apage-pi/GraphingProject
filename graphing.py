fc66d6af159ad9b6edf290fefd25ef2966def285
Adam Page <72623953+apage-pi@users.noreply.github.com>
2026-04-17 17:40:00 +0000

    fiz the graphing test

--- graphing.py
+++ graphing.py
@@ -3,4 +3,9 @@ from mathInterpereter import evaluateFunction, getXValues
 
 XValues = getXValues(-1, 1)
 YValues = evaluateFunction("x^2", -1, 1)
+equation = "x^2"
 plt.plot(XValues, YValues)
+plt.title(f"Graph of {equation}")
+plt.ylabel('Y-Axis')
+plt.xlabel('X-Axis')
+plt.show()
